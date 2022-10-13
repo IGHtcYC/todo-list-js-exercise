13b81106f7904d01c9df00dd360ae0d62a52309b
IGHtcYC <matiaselbrost@gmail.com>
2022-10-13 15:09:14 -0300

    Borramos la funcion crear tarea

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
